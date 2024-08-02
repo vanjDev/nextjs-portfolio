f9516b22d390935e92c17ee380b26d7e124922a4
vanjDev <143190477+vanjDev@users.noreply.github.com>
2024-08-03 00:23:11 +0800

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -6,22 +6,9 @@ import { Card } from "../components/card";
 
 const socials = [
 	{
-		icon: <Twitter size={20} />,
-		href: "https://twitter.com/chronark_",
-		label: "Twitter",
-		handle: "@chronark_",
-	},
-	{
-		icon: <Mail size={20} />,
-		href: "mailto:dev@chronark.com",
-		label: "Email",
-		handle: "dev@chronark.com",
-	},
-	{
-		icon: <Github size={20} />,
-		href: "https://github.com/chronark",
-		label: "Github",
-		handle: "chronark",
+		icon: <Discord size={20} />,
+		label: "AeroBlack",
+		handle: "aeroblack",
 	},
 ];
 
